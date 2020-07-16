82f7db55fe99075f4deb0040ab6a2ec96aa6b78f
DESKTOP-P20BVO6\fersyan <fersyanaxi@gmail.com>
2020-07-16 15:11:03 +0700

    Update: New Login page echan keren

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,33 +1,42 @@
-@extends('layouts.main')
+@extends('layouts/main')
 
 @section('content')
-    <div class="card-view">
+<div class="mt-5">
+  <div class="row">
+    <div class="col-sm col-md col-lg col-xl text-center"></div>
+    <div class="col-sm-8 col-md-6 col-lg-5 col-xl-4 text-center">
+      <div class="card shadow p-3 mb-5 bg-white rounded mt-4 text-center">
+        <img src="{{ asset('logo.png') }}" class="img-fluid w-75 mx-auto">
         <div class="card-body">
-            <h2>Login</h2>
-
+          <h4 class="card-title">Sistem Manajemen Arsip BKKBN Jawa Barat</h4>
+          <div class="card-text">
             {{-- Menampilkan error validasi --}}
-            @if ($errors->any())
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
-
-            <form action="/login" method="POST">
-                @csrf
-                <div class="form-group">
-                    <input class="form-control" type="text" name="username" placeholder="Username">
-                </div>
-                <div class="form-group">
-                    <input class="form-control" type="password" name="password" placeholder="Password">
-                </div>
-                <div class="form-group">
-                    <input class="btn btn-primary" type="submit" value="Login">
-                </div>
-            </form>
+              @if ($errors->any())
+                      <ul class="list-group list-group-flush">
+                          @foreach ($errors->all() as $error)
+                              <li class="list-group-item list-group-item-danger">{{ $error }}</li>
+                          @endforeach
+                      </ul>
+              @endif
+          </div>
         </div>
+        <div class="card-body">
+          <form action="/login" method="POST">
+            @csrf
+            <div class="form-group">
+                <input class="form-control" type="text" name="username" placeholder="Username">
+            </div>
+            <div class="form-group">
+                <input class="form-control" type="password" name="password" placeholder="Password">
+            </div>
+            <div class="form-group">
+                <input class="btn btn-primary" type="submit" value="Login">
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
+    <div class="col-sm col-md col-lg col-xl text-center"></div>
+  </div>
+</div>
 @endsection
--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -1,6 +1,31 @@
 @extends('layouts.main')
 
 @section('content')
+    <nav class="navbar navbar-expand-md navbar-light" style="background-color: #e3f2fd;">
+        <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="/{role_prefix}">BKKBN</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/{role_prefix}">Home</a>
+                </li>
+            </ul>
+        </div>
+        <div class="mx-auto order-0">
+            <a class="navbar-brand mx-auto" href="/{role_prefix}">Sistem Manajemen Arsip BKKBN Jawa Barat</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+        <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link btn btn-danger" href="/logout">Logout</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
     
     <nav class="navbar navbar-expand-md navbar-light bg-light">
             <ul class="navbar nav nav-pills mr-auto">
