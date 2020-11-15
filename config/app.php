8bc8fb653ad680a84263139100aaf3b956f20cd6
DESKTOP-63T5647\Fersyan <fersyanaxi@gmail.com>
2020-11-16 00:15:02 +0700

    Perbaikan title di tab browser {app.name}

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Sistem Manajemen Arsip Digital di BKKBN Provinsi Jawa Barat'),
 
     /*
     |--------------------------------------------------------------------------
